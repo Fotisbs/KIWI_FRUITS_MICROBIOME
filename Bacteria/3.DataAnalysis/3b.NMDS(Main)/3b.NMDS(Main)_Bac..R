fb35d9f9e078269c1842b1f449e3e311813fb5bf
Fotisbs <87061822+Fotisbs@users.noreply.github.com>
2025-12-09 13:50:29 +0200

    Add files via upload

---
+++ Bacteria/3.DataAnalysis/3b.NMDS(Main)/3b.NMDS(Main)_Bac..R
@@ -0,0 +1,21 @@
+##NMDS Analysis#
+##Relative abundance transformation 100%
+Bacteria_KIWI_2024_An_100 <- transform_sample_counts(Bacteria_KIWI_2024_Annotated, function(OTU) 100*OTU/sum(OTU))
+
+saveRDS(Bacteria_KIWI_2024_An_100, file = "Bacteria_KIWI_2024_An_100.RDS")
+
+##NMDS Start##
+##ordination matrix
+ord.nmds.bray1 <- ordinate(Bacteria_KIWI_2024_An_100, method="NMDS", distance="bray")
+
+##One Factor Checked
+#Tissue
+plot_ordination(Bacteria_KIWI_2024_An_100, ord.nmds.bray1, color="Tissue", shape ="", label = "", title=paste("NMDS (stress ",round(ord.nmds.bray1$stress, 2),")", sep = "")) + geom_point(size = 4) + scale_color_manual(values=c("#984EA3","#FF7F00")) + theme_bw()
+
+#MCP
+plot_ordination(Bacteria_KIWI_2024_An_100, ord.nmds.bray1, color="MCP", shape ="", label = "", title=paste("NMDS (stress ",round(ord.nmds.bray1$stress, 2),")", sep = "")) + geom_point(size = 4) + scale_color_manual(values=c("#984EA3","#FF7F00")) + theme_bw()
+
+#Cold Stor.
+plot_ordination(Bacteria_KIWI_2024_An_100, ord.nmds.bray1, color="Cold_Storage", shape ="", label = "", title=paste("NMDS (stress ",round(ord.nmds.bray1$stress, 2),")", sep = "")) + geom_point(size = 4) + scale_color_manual(values=c("#984EA3","#FF7F00","#33A02C")) + theme_bw()
+
+###NMDS END###
